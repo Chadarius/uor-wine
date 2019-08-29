fa2c0f637fc24fa72ea8fc443abe4ccb29589ac2
Chad Sutton <csutton@chadarius.com>
2019-08-29 11:53:57 -0500

    Updated to use the script directory as the WINEPREFIX to make it essentially portable.

--- uor-install.sh
+++ uor-install.sh
@@ -6,9 +6,10 @@ scriptname=`basename "$0"`
 source $scriptname.conf
 
 #Show variables
-echo UOR Wine directory is $HOME/$uordir
+echo UOR Wine directory is $uordir
 echo Wine Prefix  is $WINEPREFIX
 echo Wine Architecture is $WINEARCH
+echo Wine Debug is $WINEDEBUG
 echo Please wait while the script installs Wine and support packages.
 echo This will take a few minutes
 echo Track the install in another terminal setting with the following command
@@ -18,23 +19,19 @@ echo tail -f $scriptname.log
 exec &> $scriptname.log
 
 #Show variables
-echo UOR Wine directory is $HOME/$uordir
+echo UOR Wine directory is $uordir
 echo Wine Prefix  is $WINEPREFIX
 echo Wine Architecture is $WINEARCH
+echo Wine Debug is $WINEDEBUG
+echo Please wait while the script installs Wine and support packages.
+echo This will take a few minutes
+echo Track the install in another terminal setting with the following command
 
 #Create the UOR Wine Prefix Directory 
-if [ ! -d "$HOME/$uordir" ]; then
+if [ ! -d "$uordir" ]; then
 	echo Create the UOR Wine Prefix Directory
-	mkdir $HOME/$uordir
-#else
-	#Use this just for testing to have a clean directory each time
-	#echo Removing UOR Wine Prefix Directory for testing	
-	#rm -rf $HOME/$uordir
-	#echo Create the UOR Wine Prefix Directory
-	#mkdir $HOME/$uordir
+	mkdir "$uordir"
 fi
-
-
 
 #Install Wine 4.x
 sudo dpkg --add-architecture i386
@@ -61,9 +58,8 @@ sudo cp winetricks /usr/local/bin
 wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks.bash-completion
 sudo cp winetricks.bash-completion /usr/share/bash-completion/completions/winetricks
 
-# Setup wine directory at $HOME/$uorwineprefix
+# Setup wine directory at $uordir
 wine wineboot
-#winetricks winecfg
 winetricks -q win7
 winetricks -q corefonts
 winetricks -q msxml3
@@ -74,17 +70,36 @@ winetricks -q dotnet452
 winetricks -q dotnet48
 winetricks -q dotnet30
 
+# Create .desktop icon
+uor_icon=$(cat <<EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=UO Renaissance
+Comment=Runs Razor in Wine
+Exec=$uordir/uor
+Icon=$uordirUOLogo.png
+Path=$uordir
+Terminal=false
+StartupNotify=false
+EOF
+)
+
+echo -e "${uor_icon}" | tee "$uordir/UO Renaissance.desktop
+	
 # Copy Directories to Wine instance if they exist
 # Copy previous installs to the uor-wine script directory
 # Copy the c:\Ultima Online directory to the "Ultima Online" directory
 # Download UOR-Razor from http://www.uor-razor.com/ and unzip to "Razor" Directory
 # Copy the "c:\Program Files x86\UOAM" directory to "UOAM" directory
 if [ -d "Ultima Online" ];then
-	cp -a "Ultima Online" "$HOME/$uordir/drive_c/"
+	#cp -a "Ultima Online" "$uordir/drive_c/"
+	ln -s "$uordir/Ultima Online" "$uordir/drive_c/Ultima Online"
 fi
 
 if [ -d "UOAM" ];then
-	cp -a "UOAM" "$HOME/$uordir/drive_c/"
+	#cp -a "UOAM" "$HOME/$uordir/drive_c/"
+	ln -s "$uordir/UOAM" $uordir/drive_c/UOAM"
 fi
 
 if [ -d "Razor" ];then
